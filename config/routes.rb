a76d949ce44028e54efeecd7a86c619d2f081664
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-04-08 07:58:00 +0000

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get "lists/new"
+
   get "top" => "homes#top"
-  post "lists" => "lists#create"
-  get "lists" => "lists#index"
-  get "lists/:id" => "lists#show", as: "list"
-  get "lists/:id/edit" => "lists#edit", as:"edit_list"
-  patch "lists/:id" => "lists#update", as: "update_list"
+
+  resources :lists
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
